703f169c6fa89550e341ff5fe97a0c2706719761
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-01-08 08:33:34 +0100

    v2.5: Fixed french translation

--- src/win-about.c
+++ src/win-about.c
@@ -43,5 +43,5 @@ static void layer_header_update(Layer* layer, GContext* ctx) {
   //graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_context_set_text_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 12, GCornersAll);
-  graphics_draw_text(ctx, "Alarms++ v2.4", fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0, -2, 144, 24), GTextOverflowModeFill, GTextAlignmentCenter, 0);
+  graphics_draw_text(ctx, "Alarms++ v2.5", fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0, -2, 144, 24), GTextOverflowModeFill, GTextAlignmentCenter, 0);
 }
