63df8077338e76ce9af93f9bfa827d04f0866b9d
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-01-24 12:11:49 +0100

    v2.14: help text translated, long menu entries now scroll

--- src/win-about.c
+++ src/win-about.c
@@ -27,13 +27,13 @@ void win_about_show(void) {
 }
 
 static void window_load(Window* window) {
-  s_layer_header = layer_create(GRect(1, 1, 142, 25));
-  layer_set_update_proc(s_layer_header, layer_header_update);
-  layer_add_child(window_get_root_layer(window),s_layer_header);
+  //s_layer_header = layer_create(GRect(1, 1, 142, 24));
+  //layer_set_update_proc(s_layer_header, layer_header_update);
+  //layer_add_child(window_get_root_layer(window),s_layer_header);
 
-  s_layer_text = text_layer_create(GRect(0, 25, 144, 168 - 24));
+  s_layer_text = text_layer_create(GRect(0, 0, 144, 168));
   text_layer_set_font(s_layer_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text(s_layer_text, "Hints: long-press middle button to toggle alarm on/off. Alarms only go off when the app is closed!");
+  text_layer_set_text(s_layer_text, _("Hints: long-press middle button to toggle alarm on/off. Alarms only go off when the app is closed!"));
   layer_add_child(window_get_root_layer(window),text_layer_get_layer(s_layer_text));
 }
 
@@ -42,11 +42,11 @@ static void window_unload(Window* window) {
   text_layer_destroy(s_layer_text);
 }
 
-static void layer_header_update(Layer* layer, GContext* ctx) {
+/*static void layer_header_update(Layer* layer, GContext* ctx) {
   //graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_context_set_text_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 12, GCornersAll);
   char version_text[15];
   snprintf(version_text, sizeof(version_text), "Alarms++ v%d.%d",__pbl_app_info.process_version.major,__pbl_app_info.process_version.minor);
   graphics_draw_text(ctx, version_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0, -2, 144, 24), GTextOverflowModeFill, GTextAlignmentCenter, 0);
-}
+}*/
