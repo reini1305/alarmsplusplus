4810b597e224194d6448dcece8e7f7fcbc531e3b
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-06-11 19:10:27 +0200

    fixed am/pm should be last; added shortcut for new alarms

--- src/win-edit.c
+++ src/win-edit.c
@@ -92,26 +92,36 @@ void win_edit_show(Alarm *alarm){
   memcpy(&temp_alarm,alarm,sizeof(Alarm));
   current_alarm = alarm;
   s_select_all = false;
-  s_max[0]=1;s_min[0]=0;
-  s_max[2]=59;s_min[2]=0;
+  s_max[2]=1;s_min[2]=0;
+  s_max[1]=59;s_min[1]=0;
+  int hour;
   if(clock_is_24h_style())
   {
     s_withampm=false;
-    s_max[1]=23;s_min[1]=0;
-    s_selection = 1;
+    s_max[0]=23;s_min[0]=0;
   }
   else
   {
     s_withampm=true;
-    s_max[1]=12;s_min[1]=1;
-    int hour;
+    s_max[0]=12;s_min[0]=1;
     convert_24_to_12(temp_alarm.hour, &hour, &s_is_am);
     temp_alarm.hour = hour;
-    s_digits[0] = s_is_am;
-    s_selection = 0;
+    s_digits[2] = s_is_am;
   }
-  s_digits[1] = temp_alarm.hour;
-  s_digits[2] = temp_alarm.minute;
+  s_selection = 0;
+  if(temp_alarm.hour==0 && temp_alarm.minute==0)
+  {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    convert_24_to_12(t->tm_hour, &hour, &s_is_am);
+    s_digits[0] = clock_is_24h_style()?t->tm_hour:hour;
+    s_digits[1] = t->tm_min;
+    s_digits[2] = s_is_am;
+  } else
+  {
+    s_digits[0] = temp_alarm.hour;
+    s_digits[1] = temp_alarm.minute;
+  }
   window_stack_push(s_time_window,true);
 }
 
@@ -125,7 +135,7 @@ static void update_ui(Layer *layer, GContext *ctx) {
     if(i==s_selection)
     {
       GPoint selection_center = {
-        .x = (int16_t) (s_withampm?23:2) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING),
+        .x = (int16_t) (s_withampm?23:50) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING),
         .y = (int16_t) 50,
       };
       gpath_rotate_to(s_my_path_ptr, 0);
@@ -143,7 +153,7 @@ static void update_ui(Layer *layer, GContext *ctx) {
     if(i==s_selection)
     {
       GPoint selection_center = {
-        .x = (int16_t) (s_withampm?23:2) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING),
+        .x = (int16_t) (s_withampm?23:50) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING),
         .y = (int16_t) 50,
       };
       gpath_rotate_to(s_my_path_ptr, 0);
@@ -156,20 +166,20 @@ static void update_ui(Layer *layer, GContext *ctx) {
       gpath_draw_filled(ctx, s_my_path_ptr);
     }
 #endif
-    if(i>0)
+    if(i<2)
       snprintf(s_value_buffers[i], sizeof("00"), "%02d", s_digits[i]);
     else
-      snprintf(s_value_buffers[0], sizeof("AM"), s_digits[i]?"AM":"PM");
+      snprintf(s_value_buffers[i], sizeof("AM"), s_digits[i]?"AM":"PM");
     text_layer_set_text(s_input_layers[i], s_value_buffers[i]);
   }
-  layer_set_hidden(text_layer_get_layer(s_input_layers[0]),!s_withampm);
+  layer_set_hidden(text_layer_get_layer(s_input_layers[2]),!s_withampm);
   // draw the :
 #ifdef PBL_COLOR
   graphics_context_set_text_color(ctx,GColorDukeBlue);
 #else
   graphics_context_set_text_color(ctx,GColorBlack);
 #endif
-  graphics_draw_text(ctx,":",fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),GRect(s_withampm?144/2+20:144/2,58,40,20),
+  graphics_draw_text(ctx,":",fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),GRect(s_withampm?144/2-27:144/2,58,40,20),
                      GTextOverflowModeWordWrap,GTextAlignmentLeft,NULL);
 }
 
@@ -177,10 +187,10 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Next column
   s_selection++;
   
-  if(s_selection == 3) {
-    temp_alarm.hour = s_digits[1];
-    temp_alarm.minute = s_digits[2];
-    s_is_am = s_digits[0];
+  if(s_selection == (s_withampm? 3:2)) {
+    temp_alarm.hour = s_digits[0];
+    temp_alarm.minute = s_digits[1];
+    s_is_am = s_digits[2];
     window_stack_push(s_window,true);
   }
   else
@@ -191,7 +201,7 @@ static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Previous column
   s_selection--;
   
-  if(s_selection == (s_withampm? -1:0)) {
+  if(s_selection == -1) {
     window_stack_pop(true);
   }
   else
@@ -227,7 +237,7 @@ static void time_window_load(Window *window) {
   layer_add_child(window_layer, s_canvas_layer);
   
   for(int i = 0; i < 3; i++) {
-    s_input_layers[i] = text_layer_create(GRect((s_withampm?3:-18) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING), 60, 40, 40));
+    s_input_layers[i] = text_layer_create(GRect((s_withampm?3:30) + i * (PIN_WINDOW_SPACING + PIN_WINDOW_SPACING), 60, 40, 40));
 #ifdef PBL_COLOR
     text_layer_set_text_color(s_input_layers[i], GColorWhite);
     text_layer_set_background_color(s_input_layers[i], GColorDarkGray);
