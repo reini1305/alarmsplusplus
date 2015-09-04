a2671e0f5209b83de71333f50afcee0c8a7f6d85
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-09-04 22:22:10 +0200

    fixed 12h display bug

--- src/alarms.c
+++ src/alarms.c
@@ -200,7 +200,7 @@ bool alarm_is_one_time(Alarm *alarm)
 void convert_24_to_12(int hour_in, int* hour_out, bool* am)
 {
   *hour_out=hour_in%12;
-  if (hour_out==0) {
+  if (*hour_out==0) {
     *hour_out=12;
   }
   *am=hour_in<12;
