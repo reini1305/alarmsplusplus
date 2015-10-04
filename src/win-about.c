9e7b85da188ffee1595301cfc72fb7336402671d
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-10-04 07:57:10 +0200

    some more ui changes for chalk

--- src/win-about.c
+++ src/win-about.c
@@ -48,6 +48,14 @@ static void window_load(Window* window) {
   scroll_layer_add_child(s_layer_scroll,text_layer_get_layer(s_layer_text));
   scroll_layer_set_click_config_onto_window(s_layer_scroll, window);
   layer_add_child(window_get_root_layer(window), scroll_layer_get_layer(s_layer_scroll));
+#ifdef PBL_ROUND
+  text_layer_enable_screen_text_flow_and_paging(s_layer_text, 3);
+  text_layer_set_size(s_layer_text, GSize(window_size.size.w,500));
+  scroll_layer_set_paging(s_layer_scroll,true);
+  max_size = text_layer_get_content_size(s_layer_text);
+  text_layer_set_size(s_layer_text, max_size);
+  scroll_layer_set_content_size(s_layer_scroll, GSize(window_size.size.w, max_size.h + 12));
+#endif
 }
 
 static void window_unload(Window* window) {
@@ -57,11 +65,13 @@ static void window_unload(Window* window) {
 }
 
 static void layer_header_update(Layer* layer, GContext* ctx) {
-  //graphics_context_set_fill_color(ctx, GColorBlack);
+#ifdef PBL_COLOR
+  graphics_context_set_fill_color(ctx, GColorBlue);
+#endif
   graphics_context_set_text_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 12, GCornersAll);
   GRect layer_size = layer_get_bounds(layer);
   char version_text[15];
-  snprintf(version_text, sizeof(version_text), "Alarms++ v%d.%d",__pbl_app_info.process_version.major,__pbl_app_info.process_version.minor);
+  snprintf(version_text, sizeof(version_text), "v%d.%d",__pbl_app_info.process_version.major,__pbl_app_info.process_version.minor);
   graphics_draw_text(ctx, version_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0, -2, layer_size.size.w, 24), GTextOverflowModeFill, GTextAlignmentCenter, 0);
 }
