f6320adf387b0c9b4ab30852c88a074f3313aca1
Christian Reinbacher <reinbacher@icg.tugraz.at>
2015-12-10 09:27:49 +0100

    updated automated build process

--- make_franken.sh
+++ make_franken.sh
@@ -9,13 +9,18 @@ pebble clean --sdk 3.7 && pebble build --sdk 3.7 && cp build/*.pbw temp/37.pbw
 mv appinfo.json.old appinfo.json
 pebble clean --sdk 3.8-beta10 && pebble build --sdk 3.8-beta10 && cp build/*.pbw temp/38.pbw
 
+# replace 3.8 basalt and chalk with 3.7 versions
 cd temp
 unzip -d 37 37.pbw
 unzip -d 38 38.pbw
 rm -rf 38/basalt 38/chalk
-mv 37/basalt 37/chalk 38
+mv -f 37/basalt 37/chalk 38
 cd 38
 zip franken.zip *
+
+# put pbw in build folder
 mv franken.zip ../../build/franken.pbw
 cd ../../
+
+# clean up
 rm -rf temp
